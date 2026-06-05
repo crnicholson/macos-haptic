9b4da46d6f7cccd0979ae6eb96d2e615abb8a317
Charles Nicholson <83499056+crnicholson@users.noreply.github.com>
2026-06-05 13:42:14 -0400

    test script.

--- test.js
+++ test.js
@@ -1,4 +1,5 @@
-const { perform, patterns, getPressure, isFingerPresent } = require('./index.js');
+const { perform, patterns, allBurst, getPressure, isFingerPresent } = require('./index.js');
+
 console.log('macOS Haptic Feedback Test\nPatterns available:', patterns.join(', '), '\n');
 
 (async () => {
@@ -7,11 +8,32 @@ console.log('macOS Haptic Feedback Test\nPatterns available:', patterns.join(', 
     perform(pattern);
     await new Promise(r => setTimeout(r, 500));
   }
-  console.log('\nPolling pressure for 15 seconds. Touch the trackpad with a finger if you want to test it.\n');
+
+  console.log('\nPolling pressure for 5 seconds while triggering a burst of all patterns. Touch the trackpad with a finger if you want to test it.\n');
+
   const startedAt = Date.now();
-  while (Date.now() - startedAt < 15000) {
-    console.log({ isFingerPresent: isFingerPresent(), pressure: getPressure() });
-    await new Promise(r => setTimeout(r, 250));
+
+  while (Date.now() - startedAt < 5000) {
+    allBurst();
+    console.log("Finger Present: " + isFingerPresent() + ", Pressure: " + getPressure());
+    await new Promise(r => setTimeout(r, 100));
   }
+
+  console.log('\nTesting burst of all patterns with a gradual decrease in time between bursts.\n');
+
+  function down(i) {
+    return new Promise(resolve => {
+      function step(val) {
+        if (val < 0) { resolve(); return; }
+        allBurst();
+        console.log(`Bursting all patterns with ${val}ms delay...`);
+        setTimeout(() => step(val - 100), val);
+      }
+      step(i);
+    });
+  }
+
+  await down(1000);
+
   console.log('\nDone.');
 })();
